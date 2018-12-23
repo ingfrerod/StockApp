53568f87a142235f673ea18a04ea22c1ae6c43b3
Fredy Alberto Rodriguez Bautista <XRODF003@studenti.czu.cz>
2018-12-23 01:32:10 +0100

    Customer search datagrid done

--- StockApp/SearchCustomer.cs
+++ StockApp/SearchCustomer.cs
@@ -25,18 +25,50 @@ namespace StockApp
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(textBox1.Text.Trim()) == false)
+            dataGridView1.ClearSelection();
+            String searchValue = textBox1.Text;  //Textbox1 -> Search Key Word
+            int rowIndex = -1;
+            int counter = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[1].Value != null)
+                {
+                    if (row.Cells[1].Value.ToString().ToLower().Contains(searchValue.ToLower()))
+                    {
+                        rowIndex = row.Index;
+                        dataGridView1.Rows[rowIndex].Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = rowIndex;
+                        counter++;
+                        break;
+                    }
+                    
+                }
+                
+            }
+            if (counter == 0)
+            {
+                    MessageBox.Show(textBox1.Text + " is not in our database");
+            }
+
+            /*if (string.IsNullOrWhiteSpace(textBox1.Text.Trim()) == false)
             {
                 try
                 {
-                        dataGridView1.DataSource = DataBase.CustomerList.Select(x => x.Name == textBox1.Text.Trim());
+                    DataSet ds;
 
+                    
+                    dataGridView1.DataSource = DataBase.CustomerList.Select(x => x.Name == textBox1.Text.Trim());
+                    MessageBox.Show("Is here");
                 }
                 catch(Exception error)
                 {
                     MessageBox.Show("Eror" + error);
                 }
             }
+            else
+            {
+                MessageBox.Show("Eror");
+            }*/
         }
     }
 }
